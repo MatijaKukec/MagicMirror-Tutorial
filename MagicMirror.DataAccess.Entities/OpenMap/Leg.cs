96c1cb2743547898409abddf9d9045b8cc3b0cce
Michiel Wouters <michiel_woute64@hotmail.com>
2018-08-20 15:57:11 +0200

    Codemaid

--- MagicMirror.DataAccess.Entities/OpenMap/Leg.cs
+++ MagicMirror.DataAccess.Entities/OpenMap/Leg.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace MagicMirror.DataAccess.Entities.OpenMap
+﻿namespace MagicMirror.DataAccess.Entities.OpenMap
 {
     public class Leg
     {
